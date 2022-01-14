c10d1fb85479b071aec42aba2bf9b4979e0fcb6b
Katie Pundt <kzpundt@gmail.com>
2022-01-14 09:29:34 -0800

    update Flavor model with User property

--- PierresTreats/Models/Flavor.cs
+++ PierresTreats/Models/Flavor.cs
@@ -11,6 +11,7 @@ namespace PierresTreats.Models
 
     public int FlavorId { get; set; }
     public string Description { get; set; }
+    public virtual ApplicationUser User { get; set; }
     public virtual ICollection<FlavorTreat> JoinEntities { get; }
   }
 }
