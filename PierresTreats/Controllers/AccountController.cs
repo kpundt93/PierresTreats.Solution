0bf61465de29f7dfc3e23180643e25d0764e5e6d
Katie Pundt <kzpundt@gmail.com>
2022-01-14 13:42:22 -0800

    add Index route and corresponding Account/Index view

---
+++ PierresTreats/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using PierresTreats.Models;
+using PierresTreats.ViewModels;
+
+namespace PierresTreats.Controllers
+{
+  public class AccountController : Controller
+  {
+    private readonly PierresTreatsContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly SignInManager<ApplicationUser> _signInManager;
+
+    public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, PierresTreatsContext db)
+    {
+      _userManager = userManager;
+      _signInManager = signInManager;
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      return View();
+    }
+  }
+}
