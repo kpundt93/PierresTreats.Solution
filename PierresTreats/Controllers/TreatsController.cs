ccf19c8d4427c035a365df88279043d83152fc12
Katie Pundt <kzpundt@gmail.com>
2022-01-14 13:15:16 -0800

    add AddFlavor routes and corresponding Treats/AddFlavor view

--- PierresTreats/Controllers/TreatsController.cs
+++ PierresTreats/Controllers/TreatsController.cs
@@ -69,6 +69,24 @@ namespace PierresTreats.Controllers
       return RedirectToAction("Index");
     }
 
+    public ActionResult AddFlavor(int id, int FlavorId)
+    {
+      var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "Description");
+      return View(thisTreat);
+    }
+
+    [HttpPost]
+    public ActionResult AddFlavor(Treat treat, int FlavorId)
+    {
+      if (FlavorId != 0)
+      {
+        _db.FlavorTreats.Add(new FlavorTreat() { FlavorId = FlavorId, TreatId = treat.TreatId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
     public ActionResult Delete(int id)
     {
       var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
