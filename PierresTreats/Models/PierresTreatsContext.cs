c7573269a8b3ad0040301c5746e9061f75132066
Katie Pundt <kzpundt@gmail.com>
2022-01-14 09:10:14 -0800

    add PierresTreatsContext model

---
+++ PierresTreats/Models/PierresTreatsContext.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace PierresTreats.Models
+{
+  public class PierresTreatsContext : IdentityDbContext<ApplicationUser>
+  {
+    public DbSet<Flavor> Flavors { get; set; }
+    public DbSet<Treat> Treats { get; set; }
+    public DbSet<FlavorTreat> FlavorTreats { get; set; }
+    public PierresTreatsContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseLazyLoadingProxies();
+    }
+  }
+}
