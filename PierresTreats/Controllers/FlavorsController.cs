6fd27e8bf55b4fa5d675993c3bcc5eaf5490a3ea
Katie Pundt <kzpundt@gmail.com>
2022-01-14 11:04:27 -0800

    add Create routes and corresponding Flavors/Create view

--- PierresTreats/Controllers/FlavorsController.cs
+++ PierresTreats/Controllers/FlavorsController.cs
@@ -19,5 +19,18 @@ namespace PierresTreats.Controllers
       List<Flavor> model = _db.Flavors.ToList();
       return View(model);
     }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Flavor flavor)
+    {
+      _db.Flavors.Add(flavor);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
