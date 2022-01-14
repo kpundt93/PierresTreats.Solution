aa000d020d0522664c97b1100d852c0e05419ecf
Katie Pundt <kzpundt@gmail.com>
2022-01-14 14:07:40 -0800

    add Register routes and corresponding Account/Register view

--- PierresTreats/Controllers/AccountController.cs
+++ PierresTreats/Controllers/AccountController.cs
@@ -23,5 +23,25 @@ namespace PierresTreats.Controllers
     {
       return View();
     }
+
+    public IActionResult Register()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Register (RegisterViewModel model)
+    {
+      var user = new ApplicationUser { UserName = model.Email };
+      IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+      if (result.Succeeded)
+      {
+        return RedirectToAction("Index");
+      }
+      else
+      {
+        return View();
+      }
+    }
   }
 }
