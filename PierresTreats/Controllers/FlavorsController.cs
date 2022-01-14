40985cb94f3e6d975daa4b5f318f1c222f7af187
Katie Pundt <kzpundt@gmail.com>
2022-01-14 10:42:47 -0800

    add Index route and corresponding Flavors/Index view

---
+++ PierresTreats/Controllers/FlavorsController.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using PierresTreats.Models;
+
+namespace PierresTreats.Controllers
+{
+  public class FlavorsController : Controller
+  {
+    private readonly PierresTreatsContext _db;
+    public FlavorsController(PierresTreatsContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Flavor> model = _db.Flavors.ToList();
+      return View(model);
+    }
+  }
+}
