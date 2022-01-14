9d3bd96309216ba5600c7a869021882a7974e287
Katie Pundt <kzpundt@gmail.com>
2022-01-14 11:39:54 -0800

    add Index route and corresponding Treats/Index view

---
+++ PierresTreats/Controllers/TreatsController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using PierresTreats.Models;
+
+namespace PierresTreats.Controllers
+{
+  public class TreatsController : Controller
+  {
+    public readonly PierresTreatsContext _db;
+
+    public TreatsController(PierresTreatsContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Treat> model = _db.Treats.ToList();
+      return View(model);
+    }
+  }
+}
